31d3f45bb8f88d003263a23570dabaa879ed6c5f
waldenc <waldenc@sheridancollege.ca>
2021-08-13 21:17:30 -0400

    waldenc

---
+++ src/app/type.ts
@@ -0,0 +1,7 @@
+export class waldenc {
+    id!: number;
+    name!: string;
+    login!: string;
+    campus!: string;
+    title!: string;
+}
---
+++ src/assets/data/waldencData.ts
@@ -0,0 +1,10 @@
+import { waldenc } from "src/app/type";
+
+export const LOGINS: waldenc[] = [
+    { id: 1,
+      name: "cordelle",
+    login: "weasd",
+    campus: "davis",
+    title: "student"
+    }
+];
